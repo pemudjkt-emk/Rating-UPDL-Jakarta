e1906f563d6838df428679041ca74b1cdaee4ac2
pemudjkt-emk <pem.udjkt@gmail.com>
2026-09-02 08:10:38 +0700

    Refactor app.py for improved structure and styling

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ div[data-testid="stVerticalBlock"] > div > div {
 
 /* --- PERUBAHAN UKURAN & WARNA TEKS (1.5x LEBIH BESAR & LEBIH TERANG) --- */
 .tanya-teks {
-    font-size: 80px; /* Sebelumnya 38px, kini menjadi 57px */
+    font-size: 120px; /* Sebelumnya 38px, kini menjadi 57px */
     font-weight: 900;
     color: #1a6bb8; /* Warna biru senada header namun jauh lebih terang */
     margin-bottom: 0px;
